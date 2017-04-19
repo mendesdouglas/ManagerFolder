8f0f4d47958ce33833d8304ccecba370d21d61d7
Douglas <lixao_9@hotmail.com>
2017-04-19 20:02:46 -0300

    Efetuando teste de layout, tentativa de inserir os objetos em classes MainFrame e qtframe

--- ViewManager.py
+++ ViewManager.py
@@ -6,20 +6,37 @@ from PyQt4 import QtGui, QtCore
 class Window(QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        self.resize(796,600)
+        self.resize(796, 600)
         self.setWindowTitle("Gerenciador de Produção")
 
+        self.centralWidget = QtGui.QWidget(self)
+        self.centralWidget.setObjectName("centralwidget")
+        # inicializando
+        frame = QtGui.QFrame()
+        pmv = ProductionModelView(frame)
+
+        action_programa = QtGui.QAction("&Sair", self.centralWidget)
+        action_programa.setShortcut("Ctrl+Q")
+        action_programa.triggered.connect(pmv.productionView)
+
+
+
+
+
+
+        self.statusBar()
+
+        self.statusBar()
+        mainMenu = self.menuBar()
+        progMenu = mainMenu.addMenu('&Programa')
+        progMenu.addAction(action_programa)
+
+
+
+
         #criando um wid principal
-        self.mainwidget = QtGui.QWidget(self)
 
-        #criando um gridLayout Wid
-        self.gridLayoutWid = QtGui.QWidget(self.mainwidget)
-        self.gridLayoutWid.setGeometry(QtCore.QRect(-1,60,451,221)) #criando grid lay
 
-        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWid)
-
-        self.lb_ttlSemanal = QtGui.QLabel(self.gridLayoutWid)
-        self.lb_ttlSemanal.setText("Label")
 
         # extractAction = QtGui.QAction("&Sair", self)
         # extractAction.setShortcut("Ctrl+Q")
@@ -30,28 +47,75 @@ class Window(QtGui.QMainWindow):
         # mainMenu = self.menuBar()
         # progMenu = mainMenu.addMenu('&Programa')
         # progMenu.addAction(extractAction)
-
+        self.mainGrid = QtGui.QGridLayout()
 
 
 
         self.overView()
 
     def overView(self):
-        # btn = QtGui.QPushButton("Quit", self)
-        # btn.clicked.connect(self.close_app)
-        # btn.resize(btn.minimumSizeHint())
-        #
-        # extractAct = QtGui.QAction(QtGui.QIcon('img.png'), "Flee the scene",self)
-        # extractAct.triggered.connect(self.close_app)
-        #
-        # self.toolBar =self.addToolBar("extracao")
-        # self.toolBar.addAction(extractAct)
+
+        self.gridLay = QtGui.QWidget(self.centralWidget)
+        self.gridLay.move(500,500)
+        label1 = QtGui.QLabel(self.gridLay)
+        label1.setText("Q444h")
+
+
+       # btn = QtGui.QPushButton("Quit", self)
+
+        # label = QtGui.QLabel(self.mainGrid)
+        # label.setText("Qeheh")
+        #self.mainGrid.addWidget(label)
+
+
 
         #btn.move(100,100)
         self.show()
 
     def close_app(self):
         sys.exit()
+
+class ProductionModelView(QtGui.QFrame):
+    def __init__(self):
+        super(ProductionModelView, self).__init__()
+        self.resize(796, 600)
+        self.setWindowTitle("Gerenciador de Produção")
+
+        self.centralWidget = QtGui.QWidget(self)
+        self.centralWidget.setObjectName("centralwidget")
+
+        action_programa = QtGui.QAction("&Sair", self.centralWidget)
+        action_programa.setShortcut("Ctrl+Q")
+
+
+
+
+
+
+
+    def productionView(self):
+        print("estou passando")
+
+        self.gridLay = QtGui.QWidget(self.centralWidget)
+        self.gridLay.move(500,500)
+        label1 = QtGui.QLabel(self.gridLay)
+        label1.setText("Q444h")
+
+
+        btn = QtGui.QPushButton("Quit",self.gridLay)
+
+        # label = QtGui.QLabel(self.mainGrid)
+        # label.setText("Qeheh")
+        #self.mainGrid.addWidget(label)
+
+
+
+        #btn.move(100,100)
+        self.show()
+
+
+
+
 
 
 
